c49d4470b0bdc8dbe53ec1dddb51caf90a8f2610
Dear_Shio <darlingshio@mail.com>
2025-01-02 23:16:50 +0800

    Add build.py

---
+++ build.py
@@ -0,0 +1,12 @@
+# build.py
+from PyInstaller.__main__ import run
+
+opts = [
+    'main.py',                      # 你的主程序文件名
+    '--onefile',                    # 打包成单个exe文件
+    '--add-data=play_button.jpg;.', # 添加资源文件
+    '--uac-admin',                  # 请求管理员权限
+    '--name=AutoClicker'            # 输出的exe文件名
+]
+
+run(opts)
